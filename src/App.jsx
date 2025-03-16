79acbe76cf9387efafbe3c3a3d9f539a541b985d
AsararAhmed <connect.asarar@gmail.com>
2025-03-17 02:30:20 +0530

    Dark mode with Context api better

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import Header from './components/Header'
 import { Outlet } from 'react-router-dom'
 import { useState } from 'react'
-import { ThemeContext } from './contexts/ThemeContext'
+import { ThemeProvider } from './contexts/ThemeContext'
 
 
 import './App.css'
 
 const App = () => {
-  const [isDark, setIsDark] = useState(JSON.parse(localStorage.getItem('isDarkMode')))
 
   return (
-    <ThemeContext.Provider value={[isDark, setIsDark]}>
-      <Header  />
-      <Outlet  />
-    </ThemeContext.Provider>
+    <ThemeProvider>
+      <Header />
+      <Outlet />
+    </ThemeProvider>
   )
 }
 
